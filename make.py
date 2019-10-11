b748d8d5cdebfcbd200a7bcd5e625825991f2285
qutang <tqshelly@gmail.com>
2019-10-11 14:12:52 -0400

    Switch to use poetry build system

--- make.py
+++ make.py
@@ -1,7 +1,7 @@
 import subprocess
 import shutil
 
-# print('building documentations')
+print('building documentations')
 # build docs
 subprocess.run("pdoc --html --html-dir docs .",
                capture_output=True, shell=True)
@@ -11,3 +11,8 @@ subprocess.run('mv ./docs/arus/* ./docs/', shell=True, capture_output=True)
 
 # clean up
 subprocess.run('rmdir ./docs/arus', shell=True, capture_output=True)
+
+print('packaging...')
+
+subprocess.run("python setup.py sdist bdist_wheel",
+               shell=True, capture_output=True)
