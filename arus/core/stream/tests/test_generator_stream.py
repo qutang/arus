8e65df0de3b5acf3d6250d61a0bd787a6d9c49ba
qutang <tqshelly@gmail.com>
2019-11-20 15:16:41 +0800

    test: Add annotation generator stream test case

--- arus/core/stream/tests/test_generator_stream.py
+++ arus/core/stream/tests/test_generator_stream.py
@@ -1,23 +1,24 @@
 from ....testing import load_test_data
 from ..generator_stream import GeneratorSlidingWindowStream
-from ...accelerometer import generator
+from ...accelerometer import generator as accel_generator
+from ...annotation import generator as annot_generator
 import numpy as np
 import pandas as pd
 
 def test_GeneratorSlidingWindowStream():
     window_size = 12.8
-    sr = 50
     config = {
-        'generator': generator.normal_dist,
+        'generator': accel_generator.normal_dist,
         'kwargs': {
             "grange": 8,
             "start_time": None,
             "buffer_size": 100,
             "sleep_interval": 0,
-            "sigma": 1
+            "sigma": 1,
+            "sr": 50
         }}
     stream = GeneratorSlidingWindowStream(
-        data_source=config, sr=sr, window_size=window_size, start_time=None)
+        data_source=config, window_size=window_size, start_time=None, start_time_col=0, stop_time_col=0, name='sensor-generator-stream')
     stream.start()
     chunk_sizes = []
     chunk_means = []
@@ -28,6 +29,30 @@ def test_GeneratorSlidingWindowStream():
         n = n - 1
         if n == 0:
             break
-    assert np.all(np.array(chunk_sizes[1:-1]) == window_size * sr)
+    assert np.all(np.array(chunk_sizes[1:-1]) == window_size * config['kwargs']['sr'])
     np.testing.assert_array_almost_equal(
         np.array(chunk_means[1:-1]), 0, decimal=1)
+
+    # annotation generator stream
+    window_size = 12.8
+    config = {
+        'generator': annot_generator.normal_dist,
+        'kwargs': {
+            "duration_mu": 8,
+            "duration_sigma": 1,
+            "start_time": None,
+            "sleep_interval": 0,
+            "num_mu": 2,
+        }}
+    stream = GeneratorSlidingWindowStream(
+        data_source=config, window_size=window_size, start_time=None, start_time_col=1, stop_time_col=2, name='annotation-generator-stream')
+    stream.start()
+    chunk_durations = []
+    n = 5
+    for data, _, _, _ in stream.get_iterator():
+        durations = ((data.iloc[:, 2] - data.iloc[:, 1])/pd.Timedelta(1, 'S')).values.tolist()
+        chunk_durations += durations
+        n = n - 1
+        if n == 0:
+            break
+    np.testing.assert_array_equal(np.array(chunk_durations) <= window_size, True)
