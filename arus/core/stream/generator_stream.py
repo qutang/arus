d9ff23b781b20aed5104e47f11e18a109135f960
qutang <tqshelly@gmail.com>
2019-11-20 15:16:14 +0800

    fix: Remove `sr` from the argument list of `GeneratorSlidingWindowStream` class
    1. Users should set `sr` for sensor streams via the kwargs field in the config dict.

--- arus/core/stream/generator_stream.py
+++ arus/core/stream/generator_stream.py
@@ -13,7 +13,7 @@ class GeneratorSlidingWindowStream(SlidingWindowStream):
     The stream will generate a data stream with the generator function defined in the `data_source` and separate them into chunks specified by `window_size` to be loaded in the data queue.
     """
 
-    def __init__(self, data_source, window_size, sr, start_time=None, buffer_size=1800, simulate_reality=False, start_time_col=0, stop_time_col=0, name='generator-stream'):
+    def __init__(self, data_source, window_size, start_time=None, buffer_size=1800, simulate_reality=False, start_time_col=0, stop_time_col=0, name='generator-stream'):
         """
         Args:
             data_source (dict): a dict that stores a generator function for the simulated data and its kwargs
@@ -23,11 +23,10 @@ class GeneratorSlidingWindowStream(SlidingWindowStream):
         """
         super().__init__(data_source=data_source,
                          window_size=window_size, start_time=start_time, buffer_size=buffer_size, simulate_reality=simulate_reality, start_time_col=start_time_col, stop_time_col=stop_time_col, name=name)
-        self._sr = sr
 
     def load_data_source_(self, data_source):
         config = data_source
         generator = config['generator']
         kwargs = config['kwargs']
-        for data in generator(sr=self._sr, **kwargs):
+        for data in generator(**kwargs):
             yield data
