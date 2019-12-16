e642a209648c6ebb9f88698629e048684dfbdf1e
qutang <tqshelly@gmail.com>
2019-12-16 16:48:34 -0500

    feat: Add module for MUSS model
    1. Include class that provides functions to train, test classifier and to compute features
    2. Add scikit learn to dependency

---
+++ arus/models/muss.py
@@ -0,0 +1,127 @@
+"""
+Module of functions and classes to run MUSS model.
+
+This module includes functions to preprocess raw accelerometer data, compute features, train MUSS classifier and test with it based on the procedure described in the published manuscript.
+
+Citation: Posture and Physical Activity Detection: Impact of Number of Sensors and Feature Type (MSSE accepted)
+
+Author: Qu Tang
+Date: 2019-12-16
+License: see LICENSE file
+"""
+from functools import partial
+
+import numpy as np
+import pandas as pd
+import sklearn.svm as svm
+from sklearn.metrics import classification_report, f1_score
+from sklearn.model_selection import LeaveOneGroupOut
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.utils import shuffle
+
+from ..core.accelerometer.features import activation as accel_activation
+from ..core.accelerometer.features import orientation as accel_ori
+from ..core.accelerometer.features import spectrum as accel_spectrum
+from ..core.accelerometer.features import stats as accel_stats
+from ..core.accelerometer.transformation import vector_magnitude
+from ..core.libs.dsp.filtering import butterworth
+from ..core.libs.num import format_arr
+
+
+class MUSSModel:
+    def __init__(self, window_size=12.8, step_size=12.8):
+        self._ws = window_size
+        self._ss = step_size
+        self._saved_featuresets = []
+        self._saved_models = []
+
+    def compute_features(self, input_data, sr, st, et, subwin_secs=2, ori_unit='rad', activation_threshold=0.2):
+        subwin_samples = subwin_secs * sr
+        X = format_arr(input_data)
+        vm_feature_funcs = [
+            accel_stats.mean,
+            accel_stats.std,
+            accel_stats.max_value,
+            accel_stats.max_minus_min,
+            partial(accel_spectrum.spectrum_features,
+                    sr=sr, n=1, preset='muss'),
+            partial(accel_activation.stats_active_samples,
+                    threshold=activation_threshold)
+        ]
+
+        axis_feature_funcs = [
+            partial(accel_ori.gravity_angle_stats,
+                    subwin_samples=subwin_samples, unit=ori_unit)
+        ]
+
+        X_vm = vector_magnitude(X)
+
+        X_vm_filtered = butterworth(
+            X_vm, sr=sr, cut_offs=20, order=4, filter_type='low')
+        X_filtered = butterworth(X, sr=sr, cut_offs=20,
+                                 order=4, filter_type='low')
+
+        result = {
+            'HEADER_TIME_STAMP': [st],
+            'START_TIME': [st],
+            'STOP_TIME': [et]
+        }
+        for func in vm_feature_funcs:
+            values, names = func(X_vm_filtered)
+            if len(names) > 1:
+                for value, name in zip(values, names):
+                    result[name] = [value]
+            else:
+                result[names] = [values]
+
+        for func in axis_feature_funcs:
+            values, names = func(X_filtered)
+            if len(names) > 1:
+                for value, name in zip(values, names):
+                    result[name] = [value]
+            else:
+                result[names] = [values]
+
+        result = pd.DataFrame.from_dict(result)
+        return result
+
+    def train_classifier(self, input_features, input_classes, C=16, kernel='rbf', gamma=0.25, tol=0.0001, output_probability=True, class_weight='balanced', verbose=False, save=True):
+        """[summary]
+
+        5 better than 0.25, especially between ambulation and lying, we should use a larger gamma for higher decaying impact of neighbor samples.
+
+        Args:
+            input_features ([type]): [description]
+            input_classes ([type]): [description]
+            C (int, optional): [description]. Defaults to 16.
+            kernel (str, optional): [description]. Defaults to 'rbf'.
+            gamma (float, optional): [description]. Defaults to 0.25.
+        """
+        input_matrix, input_classes = shuffle(input_features, input_classes)
+        classifier = svm.SVC(
+            C=C,
+            kernel=kernel,
+            gamma=gamma,
+            tol=tol,
+            probability=output_probability,
+            class_weight=class_weight,
+            verbose=verbose)
+        scaler = MinMaxScaler((-1, 1))
+        scaled_X = scaler.fit_transform(input_matrix)
+        model = classifier.fit(scaled_X, input_classes)
+        train_accuracy = model.score(scaled_X, input_classes)
+        result = (model, scaler, train_accuracy)
+        if save:
+            self._saved_models.append(result)
+        return result
+
+    def predict(self, test_features, model=-1):
+        if type(model) is tuple:
+            scaler = model[1]
+            classifier = model[0]
+        elif type(model) is int:
+            scaler = self._saved_models[model][1]
+            classifier = self._saved_models[model][0]
+        scaled_X = scaler.transform(test_features)
+        predicted_classes = classifier.predict(scaled_X)
+        return predicted_classes
