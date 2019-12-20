a67049f7c94ab032f37e432e319e72173bfc1e4b
qutang <tqshelly@gmail.com>
2019-12-20 17:09:45 -0500

    test: Add test cases to validate muss classifier and to compute confusion matrix
    1. Also update other test cases to comply with the latest API changes in MUSSModel

--- arus/models/tests/test_muss.py
+++ arus/models/tests/test_muss.py
@@ -11,7 +11,6 @@ def test_muss_compute_features():
     filepath, sr = load_test_data(file_type='mhealth', sensor_type='sensor',
                                   file_num='single', exception_type='consistent_sr')
     df = read_data_csv(filepath)
-    data = df.values[:, 1:]
     st = df.iloc[0, 0]
     et = df.iloc[-1, 0]
     muss = MUSSModel()
@@ -65,30 +64,78 @@ def test_muss_combine_features():
                         ).apply(_compute_features, sr=sr)
     result = result.reset_index(drop=True)
     result2 = result.copy(deep=True)
-    combined_result = muss.combine_features(
+    combined_result, combined_feature_names = muss.combine_features(
         result, result2, placement_names=['DW', 'DA'])
     np.testing.assert_array_equal(result.columns[0:3], [
                                   'HEADER_TIME_STAMP', 'START_TIME', 'STOP_TIME'])
-    np.testing.assert_array_equal(combined_result.columns[3:], [
+    np.testing.assert_array_equal(combined_feature_names, [
                                   name + '_DW' for name in muss.get_feature_names()] + [name + '_DA' for name in muss.get_feature_names()])
     np.testing.assert_array_equal(
         combined_result.shape, (result.shape[0], 3 + len(muss.get_feature_names()) * 2))
 
 
 def test_muss_train_classifier():
+    muss = MUSSModel()
     feature_filepath, _ = load_test_data(file_type='mhealth', sensor_type='feature',
                                          file_num='single', exception_type='single_placement')
     class_filepath, _ = load_test_data(file_type='mhealth', sensor_type='class_labels',
-                                       file_num='single', exception_type='single_task')
+                                       file_num='single', exception_type='multi_tasks')
     feature_set = pd.read_csv(
         feature_filepath, infer_datetime_format=True, parse_dates=[0, 1, 2])
+    feature_set = feature_set[['HEADER_TIME_STAMP',
+                               'START_TIME', 'STOP_TIME'] + muss.get_feature_names()]
     class_set = pd.read_csv(
         class_filepath, infer_datetime_format=True, parse_dates=[0, 1, 2])
-    muss = MUSSModel()
+    class_set = class_set[['HEADER_TIME_STAMP',
+                           'START_TIME', 'STOP_TIME', 'MUSS_3_POSTURES']]
     input_feature, input_class = muss.sync_feature_and_class(
         feature_set, class_set)
-    model = muss.train_classifier(input_feature, input_class)
+    filtered_feature, filtered_class = muss.remove_classes(
+        input_feature, input_class, class_col='MUSS_3_POSTURES', classes_to_remove=['Unknown', 'Transition'])
+    model = muss.train_classifier(
+        filtered_feature, filtered_class, class_col='MUSS_3_POSTURES', feature_names=muss.get_feature_names())
     assert len(model) == 3
     assert model[2] > 0.7
     assert type(model[0]) is svm.SVC
     assert type(model[1]) is MinMaxScaler
+
+
+def test_muss_validate_classifier():
+    muss = MUSSModel()
+    feature_filepath, _ = load_test_data(file_type='mhealth', sensor_type='feature',
+                                         file_num='single', exception_type='single_placement')
+    class_filepath, _ = load_test_data(file_type='mhealth', sensor_type='class_labels',
+                                       file_num='single', exception_type='multi_tasks')
+    feature_set = pd.read_csv(
+        feature_filepath, infer_datetime_format=True, parse_dates=[0, 1, 2])
+    feature_set = feature_set[['HEADER_TIME_STAMP',
+                               'START_TIME', 'STOP_TIME', 'PID'] + muss.get_feature_names()]
+    class_set = pd.read_csv(
+        class_filepath, infer_datetime_format=True, parse_dates=[0, 1, 2])
+    class_set = class_set[['HEADER_TIME_STAMP',
+                           'START_TIME', 'STOP_TIME', 'PID', 'MUSS_3_POSTURES']]
+
+    synced_feature, synced_class = muss.sync_feature_and_class(
+        feature_set, class_set, group_col='PID')
+
+    filtered_feature, filtered_class = muss.remove_classes(
+        synced_feature, synced_class, class_col='MUSS_3_POSTURES', classes_to_remove=['Transition', 'Unknown'])
+
+    groups_to_remove = filtered_class['PID'].unique()[10:]
+    filtered_feature, filtered_class = muss.remove_groups(
+        filtered_feature, filtered_class, group_col='PID', groups_to_remove=groups_to_remove)
+
+    test_class, pred_class, acc = muss.validate_classifier(
+        filtered_feature, filtered_class, class_col='MUSS_3_POSTURES', feature_names=muss.get_feature_names(), group_col='PID')
+    np.testing.assert_array_equal(test_class.shape, pred_class.shape)
+    assert acc > 0.7
+
+
+def test_muss_get_confusion_matrix():
+    muss = MUSSModel()
+    input_class = ['Sit', 'Stand', 'Stand', 'Walk']
+    predict_class = ['Sit', 'Walk', 'Stand', 'Stand']
+    conf_df = muss.get_confusion_matrix(input_class, predict_class, labels=[
+        'Sit', 'Stand', 'Walk'], graph=False)
+    np.testing.assert_array_equal(
+        conf_df.values, np.array([[1, 0, 0], [0, 1, 1], [0, 1, 0]]))
