25c7295a874a432c64f7c3b883f425192b205bd2
qutang <tqshelly@gmail.com>
2019-12-26 18:20:32 -0500

    test: Update example for metawear stream to show case stop function

--- examples/metawear_stream.py
+++ examples/metawear_stream.py
@@ -5,8 +5,16 @@ from datetime import datetime
 if __name__ == "__main__":
     logging.basicConfig(
         level=logging.DEBUG, format='[%(levelname)s]%(asctime)s <P%(process)d-%(threadName)s> %(message)s')
-    stream = MetaWearSlidingWindowStream("D2:C6:AF:2B:DB:22", sr=50, grange=8,
-                                         window_size=5, start_time=datetime.now(), name='metawear-stream')
+    stream = MetaWearSlidingWindowStream("FF:EE:B8:99:0C:64", sr=50, grange=8,
+                                         window_size=4, start_time=datetime.now(), max_retries=10, name='metawear-stream')
     stream.start()
+    i = 0
     for data, _, _, _, _, _ in stream.get_iterator():
+        if data.empty:
+            print(data)
+            continue
+        i = i + 1
         print(data.head())
+        if i == 2:
+            break
+    stream.stop()
