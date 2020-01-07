529509d80ea0a78f386a9f51d201c5e54ba7808c
qutang <tqshelly@gmail.com>
2020-01-07 10:18:50 -0500

    fix: Add a new dev version to news.md instead of changing the top one when bumping to dev version

--- bump_version.py
+++ bump_version.py
@@ -2,13 +2,24 @@
 
 import subprocess
 import sys
-from dephell_versioning import bump_file,bump_version
-from bump_version_dev import bump_news_version
+from dephell_versioning import bump_file, bump_version
 from pathlib import Path
 import arus
 
+
+def bump_news_version(new_version):
+    new_lines = []
+    with open('news.md', 'r') as f:
+        lines = f.readlines()
+        lines[0] = '# ' + new_version + '\n'
+        new_lines = lines
+    with open('news.md', 'w') as f:
+        f.writelines(new_lines)
+
+
 if sys.argv[1] == 'major' or sys.argv[1] == 'minor' or sys.argv[1] == 'patch':
-    new_version = bump_version(version=arus.__version__, rule=sys.argv[1], scheme='semver')
+    new_version = bump_version(
+        version=arus.__version__, rule=sys.argv[1], scheme='semver')
 else:
     new_version = sys.argv[1]
 
@@ -20,7 +31,8 @@ if confirm.lower() == 'y':
     subprocess.run("poetry version " + new_version, shell=True)
 
     print('Modify package version file')
-    bump_file(path=Path('arus', '__init__.py'), old=arus.__version__, new=new_version)
+    bump_file(path=Path('arus', '__init__.py'),
+              old=arus.__version__, new=new_version)
 
     print('Modify news page version')
     bump_news_version(new_version)
@@ -31,7 +43,8 @@ if confirm.lower() == 'y':
 
     print('Tag version')
     if len(sys.argv) == 3:
-        subprocess.run('git tag -a v' + new_version + ' -m ' + '"' + sys.argv[2] + '"')
+        subprocess.run('git tag -a v' + new_version +
+                       ' -m ' + '"' + sys.argv[2] + '"')
     else:
         subprocess.run('git tag -a v' + new_version)
 
@@ -40,4 +53,3 @@ if confirm.lower() == 'y':
     subprocess.run('git push origin v' + new_version)
 else:
     exit(0)
-
--- bump_version_dev.py
+++ bump_version_dev.py
@@ -2,22 +2,24 @@
 
 import subprocess
 import sys
-from dephell_versioning import bump_file,bump_version
+from dephell_versioning import bump_file, bump_version
 from pathlib import Path
 import arus
 import os
 
-def bump_news_version(new_version):
+
+def add_news_dev_version(new_version):
     new_lines = []
     with open('news.md', 'r') as f:
         lines = f.readlines()
-        lines[0] = '# ' + new_version + '\n'
-        new_lines = lines
+        new_lines = ['# ' + new_version + '\n\n'] + lines
     with open('news.md', 'w') as f:
         f.writelines(new_lines)
 
+
 if sys.argv[1] == 'major' or sys.argv[1] == 'minor' or sys.argv[1] == 'patch':
-    new_version = bump_version(version=arus.__version__, rule=sys.argv[1], scheme='semver')
+    new_version = bump_version(
+        version=arus.__version__, rule=sys.argv[1], scheme='semver')
 else:
     new_version = sys.argv[1]
 
@@ -29,10 +31,10 @@ if confirm.lower() == 'y':
     subprocess.run("poetry version " + new_version, shell=True)
 
     print('Modify package version file')
-    bump_file(path=Path('arus', '__init__.py'), old=arus.__version__, new=new_version)
+    bump_file(path=Path('arus', '__init__.py'),
+              old=arus.__version__, new=new_version)
 
     print('Modify news page version')
-    bump_news_version(new_version)
+    add_news_dev_version(new_version)
 else:
     exit(0)
-
