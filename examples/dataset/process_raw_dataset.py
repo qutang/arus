dcbd36384831f24c1f8dfbb89108beefc67b2123
qutang <tqshelly@gmail.com>
2020-02-01 19:31:28 -0500

    refactor: Remove unnecessary codes

--- examples/dataset/process_raw_dataset.py
+++ examples/dataset/process_raw_dataset.py
@@ -9,12 +9,10 @@ Process raw dataset
 # -----------
 from arus import dataset
 from arus import developer
-import multiprocessing as mp
 
 # %%
 # process raw dataset
 # ---------------------------------
 if __name__ == "__main__":
     developer.set_default_logging()
-    mp.freeze_support()
     dataset.process_raw_dataset('spades_lab')
