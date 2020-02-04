a1e33aa8ed8aa67e456613e6ae2d0de49c017553
qutang <tqshelly@gmail.com>
2020-02-04 18:16:33 -0500

    feat: Add experiment script for RT-MUSS algorithm

--- experiments/rt_muss.py
+++ experiments/rt_muss.py
@@ -17,79 +17,155 @@
 
 # %%
 # import modules
-from arus import dataset
 import pandas as pd
 from arus.models import muss
-from arus import mhealth_format as mh
-from arus import developer
 import logging
 import os
-
-if __name__ == "__main__":
-
-    # %%
-    developer.set_default_logging()
-
-    # %%
-    # constants
-    dataset_name = 'spades_lab'
-    approach = 'muss'
-    session_name = 'SPADESInLab'
-
-    # %%
-    # variables
-    placements = ['DW', 'DA', 'DT']
-    window_sizes = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 24, 28, 32]
-    window_sizes = [2, 4, 6, 8, 10]
-
-    # %%
-    muss_model = muss.MUSSModel()
-    spades_lab = dataset.load_dataset(dataset_name)
-
-    # %% [markdown]
-    # ### Different window sizes
-
-    # %%
-    # Process spades lab datasets with different window sizes
-    for ws in window_sizes:
-        logging.info(
-            'Processing spades lab using muss with window size {}'.format(ws))
-        df = dataset.process_mehealth_dataset(
-            spades_lab, approach='muss', window_size=ws, sr=80)
-        # cache data
-        output_path = os.path.join(mh.get_processed_path(
-            spades_lab['meta']['root']), 'muss_' + str(ws) + '.csv')
-        df.to_csv(output_path, index=False)
-
+from sklearn import metrics
+import numpy as np
+import arus
 
 # %%
-# get and run validation on the dataset processed using standard
-# MUSS algorithm (window size: 12.8s)
-# d0 = pd.read_csv(spades_lab['processed']['muss'], parse_dates=[0, 1, 2])
-# p_feature_sets = []
-# for p in placements:
-#     cond = d0['PLACEMENT'] == p
-#     p_feature_set = d0.loc[cond, :]
-#     del p_feature_set['PLACEMENT']
-#     p_feature_sets.append(p_feature_set)
-# d0_combined, feature_names = dataset.combine_multi_sensor_feature_sets(
-#     *p_feature_sets,
-#     placements=placements,
-#     feature_names=muss_model.get_feature_names(),
-#     fixed_cols=['PID', 'CLASS_LABEL_' + session_name]
-# )
+
+arus.dev.set_default_logging()
+
+# constants
+dataset_name = 'spades_lab'
+approach = 'muss'
+session_name = 'SPADESInLab'
+
+# variables
+placements = ['DW', 'DA', 'DT']
+window_sizes = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 24, 28, 32]
+window_sizes = [2, 4, 6, 8, 10]
+
+muss_model = muss.MUSSModel()
+spades_lab = arus.dataset.load_dataset(dataset_name)
+
+# %% [markdown]
+# ### Different window sizes
+
+# %% Process spades lab datasets with different window sizes
 
 
-# # %%
-# d0_filtered = dataset.filter_out_column_values(
-#     d0_combined, selected_col='CLASS_LABEL_' + session_name, values_to_remove=['Unknown', 'Transition'])
-# d0_features = dataset.select_feature_set_columns(
-#     d0_filtered, selected_cols=feature_names, fixed_cols=['PID'])
-# d0_classes = dataset.select_feature_set_columns(
-#     d0_filtered, selected_cols=['CLASS_LABEL_' + session_name], fixed_cols=['PID'])
-# d0_features.shape, d0_classes.shape
+def prepare_dataset(window_sizes):
+    dataset_paths = []
+    for ws in window_sizes:
+        output_path = os.path.join(arus.mh.get_processed_path(
+            spades_lab['meta']['root']), 'muss_' + str(ws) + '.csv')
+        dataset_paths.append(output_path)
+        if os.path.exists(output_path):
+            continue
+        else:
+            logging.info(
+                'Processing spades lab using muss with window size {}'.format(ws))
+            df = arus.dataset.process_mehealth_dataset(
+                spades_lab, approach='muss', window_size=ws, sr=80)
+            # cache data
+            df.to_csv(output_path, index=False)
+    return dataset_paths
 
 
-# # %%
-# input_class_vec, output_class_vec, class_labels, acc = muss_model.validate_classifier(
-#     d0_features, d0_classes, class_col='CLASS_LABEL_' + session_name, feature_names=feature_names, placement_names=placements, group_col='PID')
+dataset_paths = prepare_dataset(window_sizes)
+
+# %% Load and prepare for validation
+
+
+def prepare_validation(window_sizes, dataset_paths, spades_lab):
+    d0 = pd.read_csv(spades_lab['processed']['muss'], parse_dates=[0, 1, 2])
+    ds = [d0] + [pd.read_csv(path, parse_dates=[0, 1, 2])
+                 for path in dataset_paths]
+    window_sizes = [12.8] + window_sizes
+    prepared_ds = []
+    for d, ws in zip(ds, window_sizes):
+        logging.info("Preparing dataset for window size: {}".format(ws))
+        p_feature_sets = []
+        for p in placements:
+            cond = d['PLACEMENT'] == p
+            p_feature_set = d.loc[cond, :]
+            del p_feature_set['PLACEMENT']
+            p_feature_sets.append(p_feature_set)
+        logging.info('  merging placement features')
+        prepared, feature_names = arus.ext.pandas.merge_all(
+            *p_feature_sets,
+            suffix_names=placements,
+            suffix_cols=muss_model.get_feature_names(),
+            on=arus.mh.FEATURE_SET_TIMESTAMP_COLS +
+            [arus.mh.FEATURE_SET_PID_COL, 'CLASS_LABEL_' + session_name],
+            how='inner',
+            sort=False
+        )
+        prepared['POSTURES'] = arus.ext.pandas.fast_series_map(
+            prepared['CLASS_LABEL_' + session_name],
+            arus.mh.transform_class_category,
+            class_category=spades_lab['meta']['class_category'],
+            input_category='FINEST_ACTIVITIES',
+            output_category='MUSS_3_POSTURES'
+        )
+        prepared['ACTIVITIES'] = arus.ext.pandas.fast_series_map(
+            prepared['CLASS_LABEL_' + session_name],
+            arus.mh.transform_class_category,
+            class_category=spades_lab['meta']['class_category'],
+            input_category='FINEST_ACTIVITIES',
+            output_category='MUSS_22_ACTIVITIES'
+        )
+        logging.info('  filtering classes')
+        prepared = arus.ext.pandas.filter_column(prepared,
+                                                 'ACTIVITIES',
+                                                 values_to_filter_out=['Unknown', 'Transition'])
+
+        features = prepared[arus.mh.FEATURE_SET_TIMESTAMP_COLS +
+                            feature_names + [arus.mh.FEATURE_SET_PID_COL]]
+        classes = prepared[arus.mh.FEATURE_SET_TIMESTAMP_COLS +
+                           ['ACTIVITIES', 'POSTURES'] + [arus.mh.FEATURE_SET_PID_COL]]
+
+        prepared_ds.append((features, classes, feature_names, ws))
+    return prepared_ds
+
+
+prepared_ds = prepare_validation(window_sizes, dataset_paths, spades_lab)
+
+# %% run validations for activities
+
+
+def run_validation(prepared_ds, class_category):
+    results = {}
+    for prepared in prepared_ds:
+        muss_model = muss.MUSSModel()
+        logging.info('Validating for window size: {} and category: {}'.format(
+            prepared[3], class_category))
+        input_class_vec, output_class_vec, class_labels, acc = muss_model.validate_classifier(
+            prepared[0], prepared[1], class_col=class_category, feature_names=prepared[2], placement_names=placements, group_col=arus.mh.FEATURE_SET_PID_COL)
+        scores = metrics.f1_score(input_class_vec, output_class_vec,
+                                  labels=class_labels, average=None)
+        mean_score = np.mean(scores)
+        if 'window_size' not in results:
+            results['window_size'] = []
+            results[class_category] = []
+            for label in class_labels:
+                results[class_category + '_' + label] = []
+        results['window_size'].append(prepared[3])
+        results[class_category].append(mean_score)
+        for label, score in zip(class_labels, scores):
+            results[class_category + '_' + label].append(score)
+    result = pd.DataFrame.from_dict(results)
+    return result
+
+
+result1 = run_validation(
+    prepared_ds, class_category='POSTURES')
+result2 = run_validation(
+    prepared_ds, class_category='ACTIVITIES')
+
+result = result1.merge(result2, on=['window_size'], how='inner')
+result = result.sort_values(by=['window_size'])
+result.to_csv(os.path.join(os.path.dirname(os.path.abspath(
+    __file__)), 'rt_muss.csv'), index=False)
+
+# %%
+result_path = os.path.join(os.path.dirname(os.path.abspath(
+    __file__)), 'rt_muss.csv')
+
+results = pd.read_csv(result_path)
+# %% plot
+results.plot(x='window_size', y='score', kind='line', ylim=[0, 1])
