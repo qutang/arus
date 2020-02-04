63599c9828985bc61d5fa3e84f52157f7b4eb077
qutang <tqshelly@gmail.com>
2020-02-04 18:16:16 -0500

    refactor: Update test cases to meet the latest api changes

--- arus/tests/test_mhealth_format.py
+++ arus/tests/test_mhealth_format.py
@@ -7,4 +7,4 @@ def test_transform_class_category(spades_lab):
     output_category = 'MUSS_3_POSTURES'
     input_label = 'Lying on the back'
     assert mh.transform_class_category(
-        class_category, input_category, input_label, output_category) == 'Lying'
+        input_label, class_category, input_category, output_category) == 'Lying'
