9fdf8615fa07d04a86f2e2c086774f9ea730641c
qutang <tqshelly@gmail.com>
2020-02-06 12:37:33 -0500

    test: Add test cases for segmentors

---
+++ arus/tests/test_segmentor.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+from .. import segmentor
+from .. import moment
+import pytest
+
+
+@pytest.fixture
+def sensor_data(spades_lab):
+    sensor_file = spades_lab['subjects']['SPADES_47']['sensors']['DW'][0]
+    df = pd.read_csv(sensor_file, header=0, parse_dates=[0])
+    return df.iloc[:1000, :]
+
+
+class TestSegmentor:
+    def test_segment(self, sensor_data):
+        df = pd.DataFrame(data=sensor_data)
+        seg = segmentor.Segmentor()
+        i = 0
+        for sample in seg.segment(df):
+            assert len(sample.shape) == 1
+            assert len(sample) == 4
+            np.testing.assert_array_equal(
+                sample.values, sensor_data.iloc[i, :].values)
+            i += 1
+
+
+class TestSlidingWindowSegmentor:
+    @pytest.mark.parametrize("window_size", [0, 1, 10])
+    @pytest.mark.parametrize('ref_st', [None, pd.Timestamp.now()])
+    def test_segment(self, sensor_data, window_size, ref_st):
+        if window_size == 0:
+            with pytest.raises(ValueError):
+                seg = segmentor.SlidingWindowSegmentor(
+                    window_size=window_size, ref_st=ref_st, st_col=0, et_col=None)
+        else:
+            seg = segmentor.SlidingWindowSegmentor(
+                window_size=window_size, ref_st=ref_st, st_col=0, et_col=None)
+            i = 0
+            for sample, st, et, prev_st, prev_et in seg.segment(sensor_data):
+                assert moment.Moment.get_duration(
+                    st, et, unit='s') == window_size
+                assert sample.shape[0] <= window_size * 80
+                assert sample.shape[1] == 4
+                i += 1
+            if ref_st is not None:
+                assert i == 0
