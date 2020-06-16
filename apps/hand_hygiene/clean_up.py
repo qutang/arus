031a5a8204f4c8ffd3e398b735bcbd86f0f96b4b
qutang <qu.tang@outlook.com>
2020-06-15 23:02:24 -0400

    feat (app): Add hand hygiene data cleaning script

--- apps/hand_hygiene/clean_up.py
+++ apps/hand_hygiene/clean_up.py
@@ -6,10 +6,27 @@ import os
 from alive_progress import alive_bar
 from loguru import logger
 import pandas as pd
+import datetime
+import shutil
+
+HAND_CLAPPING_TIME_OFFSETS = [
+    (datetime.timedelta(seconds=1, milliseconds=260),
+     datetime.timedelta(seconds=1, milliseconds=480)),
+    (datetime.timedelta(seconds=2, milliseconds=40),
+     datetime.timedelta(seconds=2, milliseconds=380)),
+    (datetime.timedelta(seconds=2, milliseconds=860),
+     datetime.timedelta(seconds=3, milliseconds=200)),
+    (datetime.timedelta(seconds=3, milliseconds=680),
+     datetime.timedelta(seconds=3, milliseconds=960)),
+    (datetime.timedelta(seconds=4, milliseconds=500),
+     datetime.timedelta(seconds=4, milliseconds=800)),
+    (datetime.timedelta(seconds=5, milliseconds=380),
+     datetime.timedelta(seconds=5, milliseconds=660))
+]
 
 
 def convert_to_mhealth(root, pid):
-    # _convert_sensors(root, pid)
+    _convert_sensors(root, pid)
     _convert_annotations(root, pid)
 
 
@@ -18,6 +35,11 @@ def _convert_sensors(root, pid):
         "Convert sensor data to mhealth format for hand hygiene raw dataset")
     actigraph_files = glob.glob(os.path.join(
         root, pid, "OriginalRaw", "*RAW.csv"), recursive=True)
+
+    master_folder = os.path.join(root, pid, arus.mh.MASTER_FOLDER)
+    logger.info('Empty existing master synced folder')
+    shutil.rmtree(master_folder)
+
     with alive_bar(len(actigraph_files)) as bar:
         for actigraph_file in actigraph_files:
             bar('Convert {} to mhealth'.format(actigraph_file))
@@ -43,17 +65,32 @@ def _convert_annotations(root, pid):
         for raw_annotation_file in raw_annotation_files:
             bar('Convert {} to mhealth'.format(raw_annotation_file))
             result_df = _read_raw_annotation_file(raw_annotation_file)
+            if result_df is None:
+                continue
             writer = arus.mh.MhealthFileWriter(
                 root, pid, hourly=True, date_folders=True)
             writer.set_for_annotation("HandHygiene", "App")
             writer.write_csv(result_df, append=False, block=True)
 
 
+def _split_hand_clapping_annotations(start_time):
+    start_time = pd.Timestamp(start_time).to_pydatetime()
+    sts = []
+    ets = []
+    for offset_st, offset_et in HAND_CLAPPING_TIME_OFFSETS:
+        st = start_time + offset_st
+        et = start_time + offset_et
+        sts.append(st)
+        ets.append(et)
+    return sts, ets
+
+
 def _read_raw_annotation_file(filepath):
     raw_df = pd.read_csv(filepath, header=None,
                          infer_datetime_format=True, parse_dates=[0])
     raw_df.columns = ['HEADER_TIME_STAMP', 'LABEL_NAME', 'EVENT_TYPE']
-    filter_condition = raw_df['LABEL_NAME'].str.contains('Collect ')
+    filter_condition = (raw_df['LABEL_NAME'].str.contains(
+        'Collect ')) & (raw_df['LABEL_NAME'].str.contains(':'))
     raw_annotations = raw_df.loc[filter_condition, :]
     logger.debug(raw_annotations)
     label_names = raw_annotations['LABEL_NAME'].unique().tolist()
@@ -61,19 +98,31 @@ def _read_raw_annotation_file(filepath):
     for label_name in label_names:
         start_times = []
         stop_times = []
-        start_times = raw_annotations.loc[(raw_annotations['LABEL_NAME'] == label_name) & (
-            raw_annotations['EVENT_TYPE'] == "START"), 'HEADER_TIME_STAMP'].values
-        stop_times = raw_annotations.loc[(raw_annotations['LABEL_NAME'] == label_name) & (
-            raw_annotations['EVENT_TYPE'] == "STOP"), 'HEADER_TIME_STAMP'].values
+        if '6 times' in label_name:
+            old_start_times = raw_annotations.loc[(raw_annotations['LABEL_NAME'] == label_name) & (
+                raw_annotations['EVENT_TYPE'] == "START"), 'HEADER_TIME_STAMP'].values
+            for st in old_start_times:
+                sts, ets = _split_hand_clapping_annotations(st)
+                start_times = start_times + sts
+                stop_times = stop_times + ets
+        else:
+            start_times = raw_annotations.loc[(raw_annotations['LABEL_NAME'] == label_name) & (
+                raw_annotations['EVENT_TYPE'] == "START"), 'HEADER_TIME_STAMP'].values
+            stop_times = raw_annotations.loc[(raw_annotations['LABEL_NAME'] == label_name) & (
+                raw_annotations['EVENT_TYPE'] == "STOP"), 'HEADER_TIME_STAMP'].values
         pruned_label_name = label_name.split(':')[1]
         label_df = pd.DataFrame(data={'HEADER_TIME_STAMP': start_times, 'START_TIME': start_times,
                                       'STOP_TIME': stop_times, 'LABEL_NAME': [pruned_label_name]*len(start_times)})
         dfs.append(label_df)
-    result_df = pd.concat(dfs, axis=0).sort_values(by=['HEADER_TIME_STAMP'])
+    if len(dfs) > 0:
+        result_df = pd.concat(dfs, axis=0).sort_values(
+            by=['HEADER_TIME_STAMP'])
+    else:
+        result_df = None
     return result_df
 
 
 if __name__ == "__main__":
-    # convert_to_mhealth('D:/datasets/hand_hygiene', 'P1')
+    convert_to_mhealth('D:/datasets/hand_hygiene', 'P1')
     arus.cli.convert_to_signaligner_both(
-        'D:/datasets/hand_hygiene', 'P1', 80, None)
+        'D:/datasets/hand_hygiene', 'P1', 80, date_range=['2020-06-09'])
---
+++ apps/hand_hygiene/hh.py
@@ -0,0 +1,48 @@
+"""hh
+
+Usage:
+  hh clean ROOT PID [SR] [--date_range=<date_range>] [--auto_range=<auto_range>]
+  hh --help
+  hh --version
+
+Arguments:
+  FOLDER                                        Dataset folder.
+  PID                                           Participant ID.
+  SR                                            Sampling rate in Hz.
+
+Options:
+  --date_range=<date_range>                     Date range. E.g., "--date_range 2020-06-01,2020-06-10", or "--date_range 2020-06-01," or "--date_range ,2020-06-10".
+  --auto_range=<auto_range>                     Auto date freq. Default is "W-SUN", or weekly starting from Sunday.
+  -h, --help                                    Show help message.
+  -v, --version                                 Program/app version.
+"""
+
+import _version
+import clean_up
+import arus
+
+from docopt import docopt
+from loguru import logger
+
+
+def main():
+    arguments = docopt(__doc__, version=f'hand hygiene {_version.__version__}')
+    logger.debug(arguments)
+    if arguments['clean']:
+        root = arguments['ROOT']
+        pid = arguments['PID']
+        sr = int(arguments['SR']) if arguments['SR'] is not None else None
+        date_range = arguments['--date_range'].split(
+            ',') if arguments['--date_range'] is not None else None
+        auto_range = arguments['--auto_range'] or "W-SUN"
+        clean_up.convert_to_mhealth(root, pid)
+        if sr is None:
+            logger.warning(
+                'Signaligner conversion is skipped because SR is None')
+        else:
+            arus.cli.convert_to_signaligner_both(
+                root, pid, sr, date_range, auto_range)
+
+
+if __name__ == "__main__":
+    main()
