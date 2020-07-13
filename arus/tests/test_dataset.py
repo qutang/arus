bdacefdf9534657e67d77b509762adfa988e3268
qutang <qu.tang@outlook.com>
2020-07-12 23:41:34 -0400

    test(dataset): Add test case for the new data classes for mhealth dataset

--- arus/tests/test_dataset.py
+++ arus/tests/test_dataset.py
@@ -3,6 +3,7 @@ from .. import env
 import os
 import shutil
 import pandas as pd
+from dataclasses import asdict
 
 
 class TestSpadesLab:
@@ -17,3 +18,16 @@ class TestSpadesLab:
         assert type(dataset_dict) == dict
         assert 'subjects' in dataset_dict.keys()
         assert 'processed' in dataset_dict.keys()
+
+
+class TestMHDataset:
+    def test_mh_dataset(self):
+        dataset_path = dataset.get_dataset_path('spades_lab')
+        mh_ds = dataset.MHDataset(
+            path=dataset_path, name='spades_lab', input_type=dataset.InputType.MHEALTH_FORMAT)
+        assert mh_ds.name == 'spades_lab'
+        assert mh_ds.subjects[0].pid == 'SPADES_1'
+        assert mh_ds.subjects[0].demography is None
+        assert mh_ds.subjects[0].sensors[0].data_type == 'AccelerationCalibrated'
+        assert mh_ds.subjects[0].annotations[0].annotation_type == 'SPADESInLab'
+        assert len(mh_ds.subjects[0].sensors[0].paths) == 3
