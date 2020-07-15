b18a709ef28110d60d2d2e553d403319748a1f82
qutang <qu.tang@outlook.com>
2020-07-14 20:22:57 -0400

    feat(model): Add method to filter out unused class labels

--- arus/models/_model.py
+++ arus/models/_model.py
@@ -10,7 +10,7 @@ from .. import error_code
 class HARModel:
     mid: str
     name: str = "HAR"
-    model: typing.Any = field(init=False)
+    model: typing.Any = None
     data_set: ds.MHDataset = field(init=False)
 
     def reset_model(self):
@@ -23,10 +23,16 @@ class HARModel:
         raise NotImplementedError(
             'This method must be implemented by sub classes!')
 
-    def compute_classes(self, **kwargs):
+    def compute_class_set(self, **kwargs):
         raise NotImplementedError(
             'This method must be implemented by sub classes!')
 
     def train(self, **kwargs):
         raise NotImplementedError(
             'This method must be implemented by sub classes!')
+
+    @staticmethod
+    def ignore_classes(fcs, task_name, remove_classes=['Unknown', 'Transition']):
+        is_valid_label = ~fcs[task_name].isin(remove_classes).values
+        fcs = fcs.loc[is_valid_label, :]
+        return fcs
