16b005008fbcd71f19dd30fb6cec02d1c6eda69c
qutang <qu.tang@outlook.com>
2020-07-18 01:28:51 -0400

    test(dataset): Add test cases for SensorObj and SGDataset

--- arus/tests/test_dataset.py
+++ arus/tests/test_dataset.py
@@ -1,9 +1,14 @@
 from .. import dataset
 from .. import env
+from .. import segmentor
+from .. import spades_lab as slab
+from .. import feature as feat
 import os
 import shutil
 import pandas as pd
 from dataclasses import asdict
+import pytest
+from loguru import logger
 
 
 class TestSpadesLab:
@@ -20,6 +25,64 @@ class TestSpadesLab:
         assert 'processed' in dataset_dict.keys()
 
 
+class TestSensorObj:
+    @pytest.mark.parametrize("seg", [None, 'sliding_window'])
+    @pytest.mark.parametrize("placement_parser", [None, slab.get_sensor_placement])
+    @pytest.mark.parametrize("stream_id", [None, "test"])
+    def test_get_stream(self, seg, placement_parser, stream_id):
+        dataset_path = dataset.get_dataset_path('spades_lab')
+        mh_ds = dataset.MHDataset(
+            path=dataset_path, name='spades_lab', input_type=dataset.InputType.MHEALTH_FORMAT)
+        mh_ds.set_placement_parser(placement_parser)
+        if seg == 'sliding_window':
+            seg = segmentor.SlidingWindowSegmentor(window_size=12)
+        stream = mh_ds.subjects[0].sensors[0].get_stream(
+            seg=seg, stream_id=stream_id, window_size=12)
+        stream.start()
+        count = 0
+        for data, context in stream.get_result():
+            logger.debug(count)
+            count += 1
+            assert data.shape[0] == 12 * 80
+            assert data.shape[1] == 4
+            if stream_id is not None:
+                assert context['data_id'] == 'test'
+            elif placement_parser is None:
+                assert context['data_id'] == 'TAS1E23150066-AccelerationCalibrated-stream'
+            else:
+                assert context['data_id'] == 'DT'
+            if count == 5:
+                logger.debug('stop')
+                stream.stop()
+
+    def test_compute_features_on_the_fly(self):
+        dataset_path = dataset.get_dataset_path('spades_lab')
+        mh_ds = dataset.MHDataset(
+            path=dataset_path, name='spades_lab', input_type=dataset.InputType.MHEALTH_FORMAT)
+        mh_ds.set_placement_parser(slab.get_sensor_placement)
+        stream = mh_ds.subjects[0].sensors[0].get_stream(
+            seg=None, stream_id=None, window_size=12.8)
+        stream.start()
+        count = 0
+        for data, context in stream.get_result():
+            logger.debug(count)
+            count += 1
+            fs = feat.FeatureSet([data], [context['data_id']], srs=[80])
+            fs.compute_per_window(
+                feat.preset, feature_names=feat.preset_names(), st=context['start_time'], et=context['stop_time'])
+            fv = fs.get_feature_set()
+            fv_names = fs.get_feature_names()
+            assert fv.iloc[0, 1].timestamp(
+            ) == context['start_time'].timestamp()
+            assert fv.iloc[0, 2].timestamp(
+            ) == context['stop_time'].timestamp()
+            assert len(fv_names) == len(feat.preset_names())
+            assert fv.shape[0] == 1
+            if count == 5:
+                logger.debug('stop')
+                stream.stop()
+
+
 class TestMHDataset:
     def test_mh_dataset(self):
         dataset_path = dataset.get_dataset_path('spades_lab')
@@ -31,3 +94,22 @@ class TestMHDataset:
         assert mh_ds.subjects[0].sensors[0].data_type == 'AccelerationCalibrated'
         assert mh_ds.subjects[0].annotations[0].annotation_type == 'SPADESInLab'
         assert len(mh_ds.subjects[0].sensors[0].paths) == 3
+        assert mh_ds.subjects[0].sensors[0].start_time.timestamp(
+        ) == pd.Timestamp('2015-09-24 14:22:48.013000').timestamp()
+
+
+class TestSGDataset:
+    def test_sg_dataset(self):
+        dataset_path = os.path.join(dataset.get_dataset_path(
+            'spades_lab'), 'SPADES_1', 'Derived', 'signaligner')
+        sg_ds = dataset.SGDataset(
+            path=dataset_path, name='spades_1_sg', sensor_folder='SPADES_1_2015092414_2015092417_sensors', label_folder='SPADES_1_2015092414_2015092417_labelsets')
+        assert sg_ds.name == 'spades_1_sg'
+        assert sg_ds.subjects[0].pid == 'spades_1_sg'
+        assert sg_ds.subjects[0].demography is None
+        assert sg_ds.subjects[0].sensors[0].data_type == 'SPADES_1_ActigraphGT9X_TAS1E23150066-AccelerationCalibrated_2015092414_2015092417.sensor'
+        assert sg_ds.subjects[0].sensors[0].sid == 'TAS1E23150066'
+        assert sg_ds.subjects[0].sensors[0].sr == 80
+        assert sg_ds.subjects[0].sensors[0].grange == 8
+        assert sg_ds.subjects[0].annotations[0].annotation_type == 'SPADES_1_diego-SPADESInLab_2015092414_2015092417.annotation'
+        assert len(sg_ds.subjects[0].sensors[0].paths) == 1
