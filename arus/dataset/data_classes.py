5de2b3098f43e6bf0ac8049e524393435a0bb230
qutang <qu.tang@outlook.com>
2020-07-18 01:28:21 -0400

    feat(dataset): Expand MHDataset, SensorObj, and AnnotationObj for data loading; Add new SGDataset for signaligner data
    1. Auto parse information for SensorObj and AnnotationObj while loading based on input type
    2. Add methods for SensorObj, AnnotationObj to get loaded data or data stream
    3. Make sure sensors and annotations are sorted while loading
    4. Now class set is categorized by annotation type instead of aid
    5. Refactor method names

--- arus/dataset/data_classes.py
+++ arus/dataset/data_classes.py
@@ -1,4 +1,5 @@
 from .. import mhealth_format as mh
+from .. import generator, plugins, segmentor, stream2
 from .. import class_label
 from dataclasses import dataclass, field
 from loguru import logger
@@ -8,25 +9,103 @@ import os
 import enum
 
 
+class InputType(enum.Enum):
+    MHEALTH_FORMAT = enum.auto()
+    SIGNALIGNER = enum.auto()
+
+
 @dataclass
 class SensorObj:
     paths: str
-    sid: str
+    sid: str = None
     data_type: str = None
     device_type: str = None
     placement: str = None
     sr: int = None
     grange: float = None
     data: pd.DataFrame = None
+    start_time: typing.Any = None
+    input_type: InputType = InputType.MHEALTH_FORMAT
+
+    def __post_init__(self):
+        if self.input_type == InputType.SIGNALIGNER:
+            reader = plugins.actigraph.ActigraphReader(
+                self.paths[0], has_ts=False, has_header=True)
+            reader.read_meta()
+            meta = reader.get_meta()
+            self.start_time = meta['START_TIME']
+            self.sr = meta['SAMPLING_RATE']
+            self.grange = meta['DYNAMIC_RANGE']
+            self.sid = meta['SENSOR_ID']
+            self.data_type = os.path.basename(self.paths[0]).split('.csv')[0]
+        elif self.input_type == InputType.MHEALTH_FORMAT:
+            self.sid = mh.parse_sensor_id_from_filepath(self.paths[0])
+            self.data_type = mh.parse_data_type_from_filepath(self.paths[0])
+            self.device_type = mh.parse_sensor_type_from_filepath(
+                self.paths[0])
+            self.start_time = mh.parse_timestamp_from_filepath_content(
+                self.paths[0])
+
+    def get_data(self):
+        if self.input_type == InputType.MHEALTH_FORMAT:
+            return mh.MhealthFileReader.read_csvs(self.paths)
+        elif self.input_type == InputType.SIGNALIGNER:
+            reader = plugins.actigraph.ActigraphReader(
+                self.paths[0], False, False)
+            reader.read()
+            return next(reader.get_data())
+
+    def get_stream(self, seg: segmentor.Segmentor = None, stream_id: str = None, **seg_kwargs):
+        if self.input_type == InputType.MHEALTH_FORMAT:
+            gen = generator.MhealthSensorFileGenerator(
+                *self.paths, buffer_size=3600 * 80)
+        elif self.input_type == InputType.SIGNALIGNER:
+            gen = plugins.actigraph.ActigraphSensorFileGenerator(
+                *self.paths, has_ts=False, has_header=True, buffer_size=3600 * 80)
+        else:
+            logger.error(
+                f'Unrecognized input type: {self.input_type.name}, failed to get stream')
+            return None
+        seg = seg or segmentor.SlidingWindowSegmentor(**seg_kwargs)
+        stream_name = self.sid + '-' + self.data_type + '-stream'
+        sensor_stream = stream2.Stream(gen, seg, name=stream_name)
+        sensor_stream.set_essential_context(
+            stream_id=stream_id or self.placement or stream_name)
+        return sensor_stream
 
 
 @dataclass
 class AnnotationObj:
     paths: str
-    aid: str
+    aid: str = None
     annotation_type: str = None
     annotator: str = None
     data: pd.DataFrame = None
+    input_type: InputType = InputType.MHEALTH_FORMAT
+
+    def __post_init__(self):
+        if self.input_type == InputType.MHEALTH_FORMAT:
+            self.annotation_type = mh.parse_annotation_type_from_filepath(
+                self.paths[0])
+            self.annotator = mh.parse_annotator_from_filepath(self.paths[0])
+            self.aid = mh.parse_data_id_from_filepath(self.paths[0])
+
+    def get_stream(self, window_size: float, start_time: "str, pandas.Timestamp", input_type: InputType):
+        if input_type == InputType.MHEALTH_FORMAT:
+            gen = generator.MhealthSensorFileGenerator(
+                *self.paths, buffer_size=3600 * 80)
+        elif input_type == InputType.SIGNALIGNER:
+            gen = plugins.actigraph.ActigraphSensorFileGenerator(
+                *self.paths, buffer_size=3600 * 80)
+        else:
+            logger.error(
+                f'Unrecognized input type: {input_type.name}, failed to get stream')
+            return None
+        seg = segmentor.SlidingWindowSegmentor(
+            window_size=window_size, ref_st=start_time, st_col=0, et_col=None)
+        sensor_stream = stream2.Stream(gen, seg, name=self.sid + '-' +
+                                       self.data_type + '-stream')
+        return sensor_stream
 
 
 @dataclass
@@ -38,9 +117,11 @@ class SubjectObj:
     annotations: typing.List[AnnotationObj] = field(default_factory=list)
     processed: typing.Dict[str, typing.Any] = field(default_factory=dict)
 
-
-class InputType(enum.Enum):
-    MHEALTH_FORMAT = enum.auto()
+    def get_sensor(self, field_name, field_value):
+        for sensor in sensors:
+            if sensor.__getattribute__(field_name) == field_value:
+                return sensor
+        return None
 
 
 @dataclass
@@ -48,23 +129,21 @@ class MHDataset:
     name: str
     path: str = None
     load: bool = True
-    input_type: InputType = None
+    input_type: InputType = InputType.MHEALTH_FORMAT
     subjects: typing.List[SubjectObj] = field(default_factory=list)
     processed: typing.Dict[str, typing.Any] = field(default_factory=dict)
-    class_func: object = None
-    placement_func: object = None
+    class_set_parser: object = None
+    placement_parser: object = None
 
     def __post_init__(self):
         if not self.load:
             return
-        if self.input_type == InputType.MHEALTH_FORMAT:
-            self._load_mhealth_format()
-        else:
-            logger.error(
-                f"Unrecognized dataset input type: {self.input_type.name}")
+        self._load()
 
-    def _load_mhealth_format(self):
+    def _load(self):
         self.subjects = []
+        if self.path is None:
+            return
         pids = mh.get_pids(self.path)
         for pid in pids:
             subj = SubjectObj(path=os.path.join(self.path, pid),
@@ -75,35 +154,29 @@ class MHDataset:
 
     def _load_sensors(self, pid):
         sensor_files = mh.get_sensor_files(pid, self.path)
-        data_ids = set(
-            map(lambda f: mh.parse_data_id_from_filepath(f), sensor_files))
+        data_ids = sorted(list(set(
+            map(lambda f: mh.parse_data_id_from_filepath(f), sensor_files))))
         sensors = []
         for data_id in data_ids:
             paths = list(filter(lambda f: data_id in f, sensor_files))
             sid = mh.parse_sensor_id_from_filepath(paths[0])
-            if self.placement_func is not None:
-                placement = self.placement_func(self.path, pid, sid)
+            if self.placement_parser is not None:
+                placement = self.placement_parser(self.path, pid, sid)
             else:
                 placement = None
-            data_type = mh.parse_data_type_from_filepath(paths[0])
-            device_type = mh.parse_sensor_type_from_filepath(paths[0])
-            sensor = SensorObj(paths=paths, sid=sid, data_type=data_type,
-                               device_type=device_type, placement=placement)
+            sensor = SensorObj(paths=paths, sid=sid, placement=placement)
             sensors.append(sensor)
         return sensors
 
     def _load_annotations(self, pid):
         annotation_files = mh.get_annotation_files(pid, self.path)
         annotations = []
-        data_ids = set(
-            map(lambda f: mh.parse_data_id_from_filepath(f), annotation_files))
+        data_ids = sorted(list(set(
+            map(lambda f: mh.parse_data_id_from_filepath(f), annotation_files))))
         for data_id in data_ids:
             paths = list(filter(lambda f: data_id in f, annotation_files))
-            annotation_type = mh.parse_annotation_type_from_filepath(
-                paths[0])
-            annotator = mh.parse_annotator_from_filepath(paths[0])
             annot = AnnotationObj(
-                paths=paths, aid=data_id, annotation_type=annotation_type, annotator=annotator)
+                paths=paths, aid=data_id)
             annotations.append(annot)
         return annotations
 
@@ -112,16 +185,16 @@ class MHDataset:
         for subj in self.subjects:
             subj.processed = {}
 
-    def set_class_func(self, class_func):
-        self.class_func = class_func
+    def set_class_set_parser(self, class_set_parser):
+        self.class_set_parser = class_set_parser
 
-    def set_placement_func(self, placement_func=None):
-        self.placement_func = placement_func
-        if placement_func is not None:
+    def set_placement_parser(self, placement_parser=None):
+        self.placement_parser = placement_parser
+        if placement_parser is not None:
             logger.info('Set placement func, loading placements...')
             for pid in self.get_pids():
                 for sensor in self.get_sensors(pid):
-                    sensor.placement = placement_func(
+                    sensor.placement = placement_parser(
                         self.path, pid, sensor.sid)
 
     def has_pid(self, pid):
@@ -199,12 +272,12 @@ class MHDataset:
 
     def get_class_set(self, pid, task_names, window_size, step_size=None, start_time=None, stop_time=None):
         step_size = step_size or window_size
-        aids = self.get_annotation_field(pid, 'aid')
+        annotation_types = self.get_annotation_field(pid, 'annotation_type')
         raw_resources = [mh.MhealthFileReader.read_csvs(
             *self.get_annotations(
-                pid, aid=aid, return_field='paths')[0], datetime_cols=[0, 1, 2]) for aid in aids]
-        class_set = class_label.ClassSet(raw_resources, aids)
-        class_set.compute_offline(window_size, self.class_func,
+                pid, annotation_type=annotation_type, return_field='paths')[0], datetime_cols=[0, 1, 2]) for annotation_type in annotation_types]
+        class_set = class_label.ClassSet(raw_resources, annotation_types)
+        class_set.compute_offline(window_size, self.class_set_parser,
                                   task_names, start_time, stop_time, step_size=step_size, pid=pid)
         return class_set.get_class_set()
 
@@ -213,8 +286,8 @@ class MHDataset:
                            input_type=self.input_type, load=False)
         for pid in pids:
             new_ds.subjects.append(self.get_subject_obj(pid))
-        new_ds.set_class_func(self.class_func)
-        new_ds.set_placement_func(self.placement_func)
+        new_ds.set_class_set_parser(self.class_set_parser)
+        new_ds.set_placement_parser(self.placement_parser)
         return new_ds
 
     def get_session_span(self, pid):
@@ -224,3 +297,67 @@ class MHDataset:
             logger.error(
                 f'Unrecognized dataset input type {self.input_type.name}')
             return None
+
+
+@dataclass
+class SGDataset(MHDataset):
+    name: str
+    path: str = None
+    sensor_folder: str = None
+    label_folder: str = None
+    load: bool = True
+    input_type: InputType = InputType.SIGNALIGNER
+    subjects: typing.List[SubjectObj] = field(default_factory=list)
+    processed: typing.Dict[str, typing.Any] = field(default_factory=dict)
+    class_set_parser: object = None
+    placement_parser: object = None
+
+    def _load(self):
+        self.subjects = []
+        pid = self.name
+        subj = SubjectObj(path=self.path, pid=pid)
+        subj.sensors = self._load_sensors(pid)
+        subj.annotations = self._load_annotations(pid)
+        self.subjects.append(subj)
+
+    def _load_sensors(self, pid):
+        sensors = []
+        if self.sensor_folder is None:
+            logger.error('sensor_folder must be set to load sensor files.')
+            return sensors
+        path = os.path.join(self.path, self.sensor_folder)
+        sensor_filenames = sorted(os.listdir(path))
+
+        for sensor_filename in sensor_filenames:
+            data_id = sensor_filename.split('.csv')[0]
+            paths = [os.path.join(path, sensor_filename)]
+            sid = data_id
+            if self.placement_parser is not None:
+                placement = self.placement_parser(self.path, pid, sid)
+            else:
+                placement = None
+            data_type = data_id
+            device_type = data_id
+            sensor = SensorObj(paths=paths, sid=sid, data_type=data_type,
+                               device_type=device_type, placement=placement, input_type=self.input_type)
+            sensors.append(sensor)
+        return sensors
+
+    def _load_annotations(self, pid):
+        annotations = []
+        if self.label_folder is None:
+            logger.error(
+                'label_folder must be set to load annotation/label files.')
+            return annotations
+        path = os.path.join(self.path, self.label_folder)
+        label_filenames = sorted(os.listdir(path))
+
+        for label_filename in label_filenames:
+            data_id = label_filename.split('.csv')[0]
+            paths = [data_id]
+            annotation_type = data_id
+            annotator = data_id
+            annot = AnnotationObj(
+                paths=paths, aid=data_id, annotation_type=annotation_type, annotator=annotator, input_type=self.input_type)
+            annotations.append(annot)
+        return annotations
