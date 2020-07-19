6cc32d2787d4e01aeca6b687083d60b92984e61e
qutang <qu.tang@outlook.com>
2020-07-18 21:05:09 -0400

    feat(models): Support prebuilt models

--- arus/models/__init__.py
+++ arus/models/__init__.py
@@ -1,2 +1,7 @@
 from ._model import *
 from .muss2 import *
+import pkg_resources
+
+
+def get_prebuilt(model_name):
+    return pkg_resources.resource_filename('arus', f'models/prebuilt/{model_name}.har')
