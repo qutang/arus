820d74876080d2bb686c28b90256287f8eef2428
qutang <qu.tang@outlook.com>
2020-07-22 11:55:46 -0400

    feat(models.muss): Support pids and step size when computing features and class sets; Support save raw data when saving model; Support cross validation

--- arus/models/muss2.py
+++ arus/models/muss2.py
@@ -1,27 +1,30 @@
 
+import gc
+import joblib
+import os
+from loguru import logger
+import pandas as pd
+from sklearn.utils import shuffle
+from sklearn import preprocessing as preprocess
+from sklearn.pipeline import make_pipeline
+import sklearn.svm as svm
+import typing
+from dataclasses import dataclass, field
+from sklearn.model_selection import TimeSeriesSplit
 from ._model import HARModel
 from .. import feature as feat
 from .. import mhealth_format as mh
 from .. import dataset as ds
 from .. import scheduler
-from dataclasses import dataclass, field
-import typing
-import sklearn.svm as svm
-from sklearn.pipeline import make_pipeline
-from sklearn import preprocessing as preprocess
-from sklearn.utils import shuffle
-import pandas as pd
-from loguru import logger
-import os
-import joblib
-import typing
-import gc
+from .splitter import TimeSeriesEpisodeSplit
+from sklearn.model_selection import cross_validate
 
 
 @dataclass
 class MUSSHARModel(HARModel):
     name: str = "MUSS_HAR"
     window_size: float = 12.8
+    step_size: float = None
     sr: int = 80
     used_placements: typing.List[str] = field(default_factory=lambda: ['DW'])
 
@@ -43,39 +46,40 @@ class MUSSHARModel(HARModel):
             subj.sensors = sensors
         self.data_set = data_set
 
-    def compute_features(self):
+    def compute_features(self, pids=None):
+        if pids is None:
+            pids = self.data_set.get_pids()
         fss = []
         sch = scheduler.Scheduler(
             scheme=scheduler.Scheduler.Scheme.SUBMIT_ORDER, max_workers=4)
         sch.reset()
-        for subj in self.data_set.subjects:
-            logger.info(f'Computing features for {subj.pid}')
-            start_time, stop_time = self.data_set.get_session_span(subj.pid)
+        for pid in pids:
+            subj = self.data_set.get_subject_obj(pid)
+            logger.info(f'Computing features for {pid}')
+            start_time, stop_time = self.data_set.get_session_span(pid)
 
             sensor_dfs, placements, srs = self._import_data_per_subj(
                 subj, self.data_set.input_type)
 
             sch.submit(
                 self._compute_features_per_subj, sensor_dfs, placements, srs,
-                start_time=start_time, stop_time=stop_time, window_size=self.window_size, sr=self.sr)
+                start_time=start_time, stop_time=stop_time, window_size=self.window_size, step_size=self.step_size, sr=self.sr)
             gc.collect()
 
-            # subj_fs, subj_fs_names = self._compute_features_per_subj(
-            #     sensor_dfs, placements, srs, start_time=start_time, stop_time=stop_time, window_size=self.window_size, sr=self.sr)
-
         result = sch.get_all_remaining_results()
-        for subj_fv_result, subj in zip(result, self.data_set.subjects):
+        for subj_fv_result, pid in zip(result, pids):
             subj_fs, subj_fs_names = subj_fv_result
+            subj = self.data_set.get_subject_obj(pid)
             if subj_fs is not None and subj_fs_names is not None:
                 subj.processed = {**subj.processed,
                                   'fs': subj_fs, 'fs_names': subj_fs_names}
                 fs = subj_fs.copy()
-                fs['PID'] = subj.pid
+                fs['PID'] = pid
                 fss.append(fs)
                 fs_names = subj_fs_names
             else:
                 logger.warning(
-                    f'Subject {subj.pid} failed to compute features, this may due to the sensor data is out of range, incorrect sensor data, or invalid sensor data values. Ignore it from the feature set.')
+                    f'Subject {pid} failed to compute features, this may due to the sensor data is out of range, incorrect sensor data, or invalid sensor data values. Ignore it from the feature set.')
         if len(fss) > 0:
             fs = pd.concat(fss, axis=0, ignore_index=True, sort=False)
             self.data_set.processed = {
@@ -83,24 +87,27 @@ class MUSSHARModel(HARModel):
         else:
             logger.warning('No feature set is computed.')
 
-    def compute_class_set(self, task_names):
+    def compute_class_set(self, task_names, pids=None):
+        if pids is None:
+            pids = self.data_set.get_pids()
         css = []
-        for subj in self.data_set.subjects:
-            logger.info(f'Compute class set for {subj.pid}')
-            start_time, stop_time = self.data_set.get_session_span(subj.pid)
+        for pid in pids:
+            subj = self.data_set.get_subject_obj(pid)
+            logger.info(f'Compute class set for {pid}')
+            start_time, stop_time = self.data_set.get_session_span(pid)
 
             subj_class_set = self.data_set.get_class_set(
-                subj.pid, task_names=task_names, window_size=self.window_size, step_size=None, start_time=start_time, stop_time=stop_time)
+                subj.pid, task_names=task_names, window_size=self.window_size, step_size=self.step_size, start_time=start_time, stop_time=stop_time)
 
             if subj_class_set is not None:
                 subj.processed = {**subj.processed,
                                   'cs': subj_class_set, 'cs_names': task_names}
                 cs = subj_class_set.copy()
-                cs['PID'] = subj.pid
+                cs['PID'] = pid
                 css.append(cs)
             else:
                 logger.warning(
-                    f'Subject {subj.pid} failed to compute class set, this may due to the annotation data is out of range, incorrect annotation data, or invalid annotation data values. Ignore it from the class set.')
+                    f'Subject {pid} failed to compute class set, this may due to the annotation data is out of range, incorrect annotation data, or invalid annotation data values. Ignore it from the class set.')
         if len(css) > 0:
             cs = pd.concat(css, axis=0, ignore_index=True, sort=False)
             self.data_set.processed = {
@@ -126,8 +133,8 @@ class MUSSHARModel(HARModel):
     def get_training_acc(self):
         return self.train_perf['acc']
 
-    def train(self, task_name, ignore_classes=["Unknown", "Transition"], pids=[], verbose=False, **kwargs):
-        if len(pids) == 0:
+    def train(self, task_name, ignore_classes=["Unknown", "Transition"], pids=None, verbose=False, **kwargs):
+        if pids is None:
             pid_info = 'all participants'
         else:
             pid_info = ','.join(pids)
@@ -148,6 +155,7 @@ class MUSSHARModel(HARModel):
             X, y, verbose=verbose, **kwargs)
         self.train_perf = {**self.train_perf, 'acc': train_acc}
         logger.info(f'Training accuracy: {train_acc}')
+        self.train_pids = pids
 
     def save_fcs(self, pids=[]):
         fcs, fs_names, cs_names = self._preprocess_feature_class_set(pids)
@@ -159,15 +167,16 @@ class MUSSHARModel(HARModel):
         else:
             logger.warning('No available feature and class set to save!')
 
-    def save_model(self, save_fcs=True):
+    def save_model(self, save_raw=True, save_fcs=True):
         output_filepath = os.path.join(
-            self.get_processed_path(), self.name + '.har')
+            self.get_processed_path(), self.name + '-' + '-'.join(self.train_pids) + '.har')
         logger.info(f'Save trained MUSS model to {output_filepath}')
         bundle = {
             'mid': self.mid,
             'placements': self.used_placements,
             'model': self.model,
             'dataset_name': self.data_set.name,
+            'dataset': self.data_set if save_raw else None,
             'fs': None if not save_fcs else self.data_set.processed['fs'],
             'cs': None if not save_fcs else self.data_set.processed['cs'],
             'fs_names': None if not save_fcs else self.data_set.processed['fs_names'],
@@ -181,7 +190,10 @@ class MUSSHARModel(HARModel):
         model = MUSSHARModel(
             mid=bundle['mid'], used_placements=bundle['placements'])
         model.model = bundle['model']
-        model.data_set = ds.MHDataset(name=bundle['dataset_name'])
+        if 'dataset' in bundle:
+            model.data_set = bundle['dataset']
+        else:
+            model.data_set = ds.MHDataset(name=bundle['dataset_name'])
         model.data_set.processed['fs'] = bundle['fs']
         model.data_set.processed['cs'] = bundle['cs']
         model.data_set.processed['fs_names'] = bundle['fs_names']
@@ -207,6 +219,26 @@ class MUSSHARModel(HARModel):
             raise ValueError(
                 f'Unrecognized input type {type(input_objs[0])} for predict function.')
         return result
+
+    def cross_validation(self, task_name, pids=None, n_fold=5, **kwargs):
+        if pids is None:
+            pids = self.data_set.get_pids()
+        fcs, fs_names, fc_names = self._preprocess_feature_class_set(pids=pids)
+        fcs = self.ignore_classes(fcs, task_name=task_name, remove_classes=[
+                                  'Unknown', 'Transition', 'Transition-Left', 'Transition-Right', 'Transition-Both', 'Unknown-Left', 'Unknown-Right', 'Unknown-Both'])
+        fcs.reset_index(drop=True, inplace=True)
+        splitter = TimeSeriesEpisodeSplit(
+            fcs, fs_names, task_name, n_split=n_fold)
+        clf = self.get_estimator(**kwargs)
+        scores = cross_validate(clf, fcs.loc[:, fs_names].values,
+                                y=fcs.loc[:, task_name].values, cv=splitter.split(), scoring=['precision_macro', 'recall_macro', 'f1_macro', 'accuracy'], n_jobs=4, return_train_score=True)
+        cv_df = pd.DataFrame.from_dict(scores)
+        self.data_set.processed = {
+            **self.data_set.processed,
+            'cv': cv_df
+        }
+        logger.info(cv_df)
+        logger.info(cv_df.describe())
 
     def _predict_subj(self, *subj_objs: typing.List[ds.SubjectObj]):
         results = []
@@ -264,9 +296,9 @@ class MUSSHARModel(HARModel):
         })
         return predict_df
 
-    def _preprocess_feature_class_set(self, pids):
+    def _preprocess_feature_class_set(self, pids=None):
         if 'fs' in self.data_set.processed:
-            if len(pids) > 0:
+            if pids is not None:
                 fss = []
                 for pid in pids:
                     fs = self.data_set.get_subject_obj(pid).processed['fs']
@@ -283,7 +315,7 @@ class MUSSHARModel(HARModel):
                 'No feature set available, please run compute_features at first.')
             return
         if 'cs' in self.data_set.processed:
-            if len(pids) > 0:
+            if pids is not None:
                 css = []
                 for pid in pids:
                     cs = self.data_set.get_subject_obj(pid).processed['cs']
@@ -308,8 +340,15 @@ class MUSSHARModel(HARModel):
                        on=on, sort=False)
         return fcs, fs_names, cs_names
 
-    def _train_classifier(self, X, y, C=16, kernel='rbf', gamma=0.25, tol=0.0001, output_probability=True, class_weight='balanced', verbose=False):
+    def _train_classifier(self, X, y, **kwargs):
         input_matrix, input_classes = shuffle(X, y)
+        pipe = self.get_estimator(**kwargs)
+        logger.debug(pipe)
+        pipe.fit(input_matrix, input_classes)
+        train_accuracy = pipe.score(input_matrix, input_classes)
+        return pipe, train_accuracy
+
+    def get_estimator(self, C=16, kernel='rbf', gamma=0.25, tol=0.0001, output_probability=True, class_weight='balanced', verbose=False):
         classifier = svm.SVC(
             C=C,
             kernel=kernel,
@@ -319,14 +358,10 @@ class MUSSHARModel(HARModel):
             class_weight=class_weight,
             verbose=verbose
         )
-
         scaler = preprocess.MinMaxScaler((-1, 1))
         pipe = make_pipeline(scaler, classifier,
                              memory=self.get_processed_path())
-        logger.debug(pipe)
-        pipe.fit(input_matrix, input_classes)
-        train_accuracy = pipe.score(input_matrix, input_classes)
-        return pipe, train_accuracy
+        return pipe
 
     def _import_data_per_subj(self, subj, input_type: ds.InputType):
         sensor_dfs = []
@@ -346,6 +381,7 @@ class MUSSHARModel(HARModel):
     def _compute_features_per_subj(self, sensor_dfs, placements, srs, **kwargs):
         feature_set = feat.FeatureSet(sensor_dfs, placements, srs)
         feature_set.compute_offline(window_size=kwargs['window_size'],
+                                    step_size=kwargs['step_size'],
                                     feature_func=feat.preset,
                                     feature_names=feat.preset_names(),
                                     sr=kwargs['sr'],
