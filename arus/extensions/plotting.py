716d23b0c72ab3e061150d6f3e76fd788ba6e2aa
qutang <qu.tang@outlook.com>
2020-07-23 17:01:06 -0400

    feat(ext.plotting): Plot publication quality confusion matrix

--- arus/extensions/plotting.py
+++ arus/extensions/plotting.py
@@ -1,5 +1,8 @@
 import matplotlib.colors as mc
 import colorsys
+from matplotlib import rcParams, rcParamsDefault
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 
 def adjust_lightness(color, amount=0.5, return_format='rgb'):
@@ -15,3 +18,20 @@ def adjust_lightness(color, amount=0.5, return_format='rgb'):
         return mc.to_hex(result)
     else:
         return result
+
+
+def confusion_matrix(cm_df):
+    rcParams.update(rcParamsDefault)
+    rcParams['font.family'] = 'serif'
+    rcParams['font.size'] = 10
+    rcParams['font.serif'] = ['Times New Roman']
+    fig, ax = plt.subplots(figsize=(8, 8))
+    sns.set_style({
+        'font.family': 'serif',
+        'font.size': 10
+    })
+    g = sns.heatmap(cm_df, annot=True, cmap="Greys",
+                    cbar=False, fmt='d', robust=True, linewidths=0.2)
+    g.set(xlabel="Prediction", ylabel="Ground truth")
+    plt.tight_layout()
+    return fig
