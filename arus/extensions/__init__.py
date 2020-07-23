ec9e49b59bcbb155610e45e35d12c9dbe06e0fd1
qutang <qu.tang@outlook.com>
2020-07-23 17:02:03 -0400

    feat(ext.sklearn): Support return cross validation predictions for any cv methods

--- arus/extensions/__init__.py
+++ arus/extensions/__init__.py
@@ -2,3 +2,4 @@ from . import pandas
 from . import numpy
 from . import filesys
 from . import plotting
+from . import sklearn
---
+++ arus/extensions/sklearn.py
@@ -0,0 +1,27 @@
+from joblib import Parallel, delayed
+
+
+def _val_predict(clf, X_train, y_train, X_test, y_test, fit_params=None, method='predict'):
+    fit_params = fit_params or {}
+    clf.fit(X_train, y_train, **fit_params)
+    predict = getattr(clf, method)
+    y_pred = predict(X_test)
+    return y_test, y_pred
+
+
+def cross_val_predict(clf, X, y, groups=None, cv=None, n_jobs=None, fit_params=None, method='predict'):
+    with Parallel(n_jobs=n_jobs) as parallel:
+        results = parallel(delayed(_val_predict)(
+            clf=clf,
+            X_train=X[train_idx, :],
+            y_train=y[train_idx],
+            X_test=X[test_idx, :],
+            y_test=y[test_idx],
+            fit_params=fit_params,
+            method=method) for train_idx, test_idx in cv)
+    preds = []
+    tests = []
+    for test, pred in results:
+        tests += test.tolist()
+        preds += pred.tolist()
+    return tests, preds
