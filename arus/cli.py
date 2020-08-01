05a1284f3eddd12a3380b26004fec008afbb393e
qutang <qu.tang@outlook.com>
2020-08-01 18:50:11 -0400

    refactor: Remove preset command from command line app

--- arus/cli.py
+++ arus/cli.py
@@ -4,7 +4,6 @@ Usage:
   arus signaligner FOLDER PID [SR] [-t <file_type>] [--date_range=<date_range>] [--auto_range=<auto_range>] [--debug]
   arus app APP_COMMAND FOLDER NAME [--app_version=<app_version>]
   arus dataset DATASET_COMMAND DATASET_NAME [FOLDER] [OUTPUT_FOLDER] [--debug]
-  arus preset PRESET_COMMAND MODEL_NAME [TEST_FILE] [--file-format=<format>] [--placements=<placements>] [--target=<target>] [--debug]
   arus package PACK_COMMAND [NEW_VERSION] [--dev] [--release] [--debug]
   arus --help
   arus --version
@@ -29,11 +28,10 @@ Options:
 
 from docopt import docopt
 from loguru import logger
-from . import developer
+from . import dev as dev
 from . import dataset as ds
 from . import mhealth_format as mh
 from . import plugins
-from . import feature as feat
 from . import models
 from . import spades_lab as slab
 from . import env
@@ -54,7 +52,7 @@ import pkg_resources
 
 def cli():
     arguments = docopt(__doc__, version='arus {}'.format(
-        developer._find_current_version(".", "arus")))
+        dev._find_current_version(".", "arus")))
     if arguments['--debug']:
         logger.remove()
         logger.add(sys.stderr, level='DEBUG')
@@ -69,8 +67,6 @@ def cli():
         app_command(arguments)
     elif arguments['dataset']:
         dataset_command(arguments)
-    elif arguments['preset']:
-        preset_command(arguments)
     elif arguments['package']:
         package_command(arguments)
 
@@ -159,9 +155,9 @@ def app_command(arguments):
     if arguments['APP_COMMAND'] == 'build':
         root = arguments['FOLDER']
         name = arguments['NAME']
-        version = arguments['--app_version'] or developer._find_current_version(
+        version = arguments['--app_version'] or dev._find_current_version(
             '.', 'arus')
-        developer.build_arus_app(root, name, version)
+        dev.build_arus_app(root, name, version)
     elif arguments['APP_COMMAND'] == 'run':
         root = arguments['FOLDER']
         name = arguments['NAME']
@@ -176,7 +172,7 @@ def dataset_command(arguments):
         output_folder = arguments['OUTPUT_FOLDER']
         logger.info(
             f'Compressing dataset as {name}.tar.gz in folder {output_folder}')
-        developer.compress_dataset(root, output_folder, f'{name}.tar.gz')
+        dev.compress_dataset(root, output_folder, f'{name}.tar.gz')
     elif arguments['DATASET_COMMAND'] == 'query':
         name = arguments['DATASET_NAME']
         if name == 'all':
@@ -187,54 +183,11 @@ def dataset_command(arguments):
             logger.info(ds.get_sample_datapath(name))
 
 
-def preset_command(arguments):
-    if arguments['PRESET_COMMAND'] == 'train':
-        model_name = arguments['MODEL_NAME']
-        if model_name == 'muss':
-            target_name = arguments['--target']
-            placements = arguments['--placements'].split(',')
-            _train_muss(placements, target_name)
-    elif arguments['PRESET_COMMAND'] == 'predict':
-        model_path = arguments['MODEL_NAME']
-        test_file = arguments['TEST_FILE']
-        file_format = arguments['--file-format']
-        predict_df, output_path = predict_model(
-            model_path, test_file, file_format)
-        plugins.signaligner.save_as_signaligner(
-            predict_df, output_path, plugins.signaligner.FileType.ANNOTATION, labelset=model.name, mode='w', index=False, header=True)
-
-
 def format_time(ts):
     s = ts.strftime('%Y-%m-%d %H:%M:%S')
     ms = '{:03.0f}'.format(math.floor(ts.microsecond / 1000.0))
     result = "{}.{}".format(s, ms)
     return result
-
-
-def _train_muss(placements, target_name):
-    spades_lab = ds.MHDataset(name='spades_lab',
-                              path=ds.get_dataset_path('spades_lab'))
-    spades_lab.set_placement_parser(slab.get_sensor_placement)
-    spades_lab.set_class_set_parser(slab.class_set)
-    mid = '-'.join(placements) + '-' + target_name + '-muss'
-    model = models.MUSSHARModel(mid=mid, used_placements=placements)
-    model.load_dataset(spades_lab)
-    model.compute_features()
-    model.compute_class_set(task_names=[target_name])
-    model.train(target_name)
-    model.get_training_acc()
-    model.save_model()
-
-
-def predict_model(model_path, test_file, file_format):
-    model = models.MUSSHARModel.load_model(model_path)
-    if file_format == 'signaligner':
-        predict_df = model.predict(test_file,
-                                   placements=model.used_placements,
-                                   file_format=ds.InputType.SIGNALIGNER)
-        output_path = os.path.join(os.path.dirname(
-            test_file), model.mid + '.prediction.csv')
-        return predict_df, output_path
 
 
 def package_command(arguments):
@@ -248,32 +201,15 @@ def package_command(arguments):
         is_dev = arguments['--dev'] or True
         release = arguments['--release'] or False
         if is_dev and not release:
-            developer.dev_website()
+            dev.dev_website()
         elif release:
-            developer.build_website()
-    elif arguments['PACK_COMMAND'] == 'update-prebuilt':
-        _update_prebuilt_models()
-
-
-def _update_prebuilt_models():
-    logger.info('Update prebuilt models...')
-    _train_muss(['NDW'], 'INTENSITY')
-    # copy model to prebuilt folder
-    src = os.path.join(env.get_data_home(), 'spades_lab',
-                       mh.PROCESSED_FOLDER, 'NDW-INTENSITY-muss', 'MUSS_HAR.har')
-    dest = os.path.join(pkg_resources.resource_filename(
-        'arus', 'models/prebuilt'), 'intensity.har')
-    if os.path.exists(dest):
-        logger.info('Remove previous models...')
-        os.remove(dest)
-    shutil.copyfile(src, dest)
-    logger.info('Updated prebuilt models...')
+            dev.build_website()
 
 
 def _release_package(nver, dev=False, release=False):
-    new_version = developer.bump_package_version('.', 'arus', nver, dev)
-    if new_version is not None and developer.command_is_available('git') and release:
-        developer.commit_repo(f"Bump version to {new_version}")
-        developer.tag_repo(new_version)
-        developer.push_repo()
-        developer.push_tag(new_version)
+    new_version = dev.bump_package_version('.', 'arus', nver, dev)
+    if new_version is not None and dev.command_is_available('git') and release:
+        dev.commit_repo(f"Bump version to {new_version}")
+        dev.tag_repo(new_version)
+        dev.push_repo()
+        dev.push_tag(new_version)
