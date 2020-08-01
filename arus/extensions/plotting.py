186fdb137de5ae5c80fd36470fe53aff225cc0e8
qutang <qu.tang@outlook.com>
2020-08-01 00:09:11 -0400

    feat: Add function to draw learning curve

--- arus/extensions/plotting.py
+++ arus/extensions/plotting.py
@@ -3,6 +3,7 @@ import colorsys
 from matplotlib import rcParams, rcParamsDefault
 from matplotlib import pyplot as plt
 import seaborn as sns
+import numpy as np
 
 
 def adjust_lightness(color, amount=0.5, return_format='rgb'):
@@ -34,4 +35,57 @@ def confusion_matrix(cm_df):
                     cbar=False, fmt='d', robust=True, linewidths=0.2)
     g.set(xlabel="Prediction", ylabel="Ground truth")
     plt.tight_layout()
+    return fig
+
+
+def learning_curve(lc_df, title='Learning curve', axes=None, ylim=None):
+    if axes is None:
+        fig, axes = plt.subplots(1, 3, figsize=(20, 5))
+
+    axes[0].set_title(title)
+    if ylim is not None:
+        axes[0].set_ylim(*ylim)
+    axes[0].set_xlabel("Training examples")
+    axes[0].set_ylabel("Score")
+
+    train_sizes = lc_df['train_sizes']
+    train_scores_mean = lc_df['train_scores_means']
+    train_scores_std = lc_df['train_scores_stds']
+    test_scores_mean = lc_df['test_scores_means']
+    test_scores_std = lc_df['test_scores_stds']
+    fit_times_mean = lc_df['fit_times_means']
+    fit_times_std = lc_df['fit_times_stds']
+
+    # Plot learning curve
+    axes[0].grid()
+    axes[0].fill_between(train_sizes, train_scores_mean - train_scores_std,
+                         train_scores_mean + train_scores_std, alpha=0.1,
+                         color="r")
+    axes[0].fill_between(train_sizes, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1,
+                         color="g")
+    axes[0].plot(train_sizes, train_scores_mean, 'o-', color="r",
+                 label="Training score")
+    axes[0].plot(train_sizes, test_scores_mean, 'o-', color="g",
+                 label="Cross-validation score")
+    axes[0].legend(loc="best")
+
+    # Plot n_samples vs fit_times
+    axes[1].grid()
+    axes[1].plot(train_sizes, fit_times_mean, 'o-')
+    axes[1].fill_between(train_sizes, fit_times_mean - fit_times_std,
+                         fit_times_mean + fit_times_std, alpha=0.1)
+    axes[1].set_xlabel("Training examples")
+    axes[1].set_ylabel("fit_times")
+    axes[1].set_title("Scalability of the model")
+
+    # Plot fit_time vs score
+    axes[2].grid()
+    axes[2].plot(fit_times_mean, test_scores_mean, 'o-')
+    axes[2].fill_between(fit_times_mean, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1)
+    axes[2].set_xlabel("fit_times")
+    axes[2].set_ylabel("Score")
+    axes[2].set_title("Performance of the model")
+
     return fig
