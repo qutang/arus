dd7ad86dace586a64fd9ace51df157a04d821c4e
qutang <qu.tang@outlook.com>
2020-07-31 22:49:08 -0400

    tests: Add new package level test data

--- arus/tests/conftest.py
+++ arus/tests/conftest.py
@@ -1,7 +1,23 @@
+import pandas as pd
 import pytest
-from .. import dataset
+
+from .. import dataset as ds
 
 
 @pytest.fixture(scope="module")
 def spades_lab_data():
-    return dataset.load_dataset('spades_lab')
+    return ds.load_dataset('spades_lab')
+
+
+@pytest.fixture(scope="module")
+def spades_lab_ds():
+    return ds.MHDataset(path=ds.get_dataset_path('spades_lab'),
+                        name='spades_lab', input_type=ds.InputType.MHEALTH_FORMAT)
+
+
+@pytest.fixture(scope="module")
+def single_mhealth_sensor(spades_lab_ds):
+    sensor = spades_lab_ds.get_sensors(pid='SPADES_1', placement='DW')[0]
+    sensor_file = sensor.paths[0]
+    data = pd.read_csv(sensor_file, parse_dates=[0])
+    return data
