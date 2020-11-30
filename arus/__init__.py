3baa4da8cd9f7c564c321917195685833fc057bf
qutang <qu.tang@outlook.com>
2020-11-29 20:48:23 -0500

    Bump version to 1.1.19

--- arus/__init__.py
+++ arus/__init__.py
@@ -3,7 +3,7 @@ __arus__ python package provides a computation framework to manage and process u
 
 """
 
-__version__ = '1.1.18'
+__version__ = '1.1.19'
 
 from . import accelerometer as accel
 from . import cli
